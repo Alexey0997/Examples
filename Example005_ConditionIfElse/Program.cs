596617cf9536d4c6fc4be2fdd05a25f569d38bc4
alexey <asivlev@yandex.ru>
2022-10-14 02:22:06 +0300

    Add Condition If-Else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿// Программа для особого приветствия Валюшки
+Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+if (username.ToLower() == "Валюшка")
+{
+    Console.WriteLine("Ура, это же Валюшка!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine("username!");
+}
