4cfcd11af4f8e0d58a4a92be12713e2e2419b7cd
alexey <asivlev@yandex.ru>
2022-10-14 20:49:17 +0300

    square number

---
+++ Seminar1_Sqr/Program.cs
@@ -0,0 +1,6 @@
+﻿// Программа рассчета квадрата числа, введенного пользователем. 
+Console.Clear();
+Console.WriteLine("Введите целое число: ");
+int number = int.Parse(Console.ReadLine());
+int sqw = number * number;
+Console.WriteLine($"квадрат числа {number} = {sqw}");
