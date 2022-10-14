f975f3742d6e8f667cc325f983cdffc6cd861084
alexey <asivlev@yandex.ru>
2022-10-14 12:43:00 +0300

    Add Random and result

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,4 +1,7 @@
 ﻿// Программа для сложения двух целых чисел
-int numberA = 125;
-int numberB = 1000;
-Console.WriteLine(numberA + numberB);
+int numberA = new Random().Next(1,10); // используем герератор случайных чисел для задания А [0,10)
+Console.WriteLine(numberA); // Выведем значение А в терминале
+int numberB = new Random().Next(1,10); // используем герератор случайных чисел для задания B [0,10)
+Console.WriteLine(numberB); // Выведем значение B в терминале
+int result = numberA + numberB; // Вводим значение result для обозначения суммы
+Console.WriteLine(result); // Выводим результат вычислений на печать
