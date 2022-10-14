ce0718d3b987ba880a21b7b7ec3aaad26d7023f6
alexey <asivlev@yandex.ru>
2022-10-15 00:02:10 +0300

    numbers

---
+++ Seminar1_Numbers/Program.cs
@@ -0,0 +1,11 @@
+﻿// Написать программу, которая на вход принимает одно число (N), 
+// а на выходе показывает все целые числа в промеждутке от -N до N.
+Console.Clear();
+Console.WriteLine("Введите целое число: ");
+int number = int.Parse(Console.ReadLine());
+int count = number * -1;
+while(count <= number)
+{
+    Console.Write($"{count}, ");
+    count++;
+}
