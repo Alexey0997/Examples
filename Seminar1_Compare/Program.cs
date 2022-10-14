d3ca957c717d4c766ff6152c55b262c9e4361446
alexey <asivlev@yandex.ru>
2022-10-14 21:17:04 +0300

    Add comparing

---
+++ Seminar1_Compare/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+Console.Clear();
+Console.WriteLine("Введи первое целое и положительное число: ");
+int numberA = int.Parse(Console.ReadLine());
+Console.WriteLine("Введи второе целое и положительное число: ");
+int numberB = int.Parse(Console.ReadLine());
+int sqw = numberB * numberB;
+if(numberA == sqw)
+{
+    Console.WriteLine($"Первое число {numberA} является квадратом второго числа {numberB}.");
+}
+else
+{
+    Console.WriteLine("Первое число не является квадратом второго.");
+}
